e964eede674b718f03ae48ee3317df6ae107f934
yikezawa <y.ikezawa93@gmail.com>
2020-10-09 22:55:47 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,24 +4,52 @@ using namespace std;
 
 const long long INF = 1LL << 60;
 
+template<class T> void chmin(T& a, T b){
+    if (a > b){
+        a = b;
+    }
+}
+
 int main(){
     int N;
     cin >> N;
-    vector<long long>h(N);
+    vector<long long> h(N);
     for (int i = 0; i < N; ++i) cin >> h[i];
 
     vector<long long> dp(N, INF);
 
     dp[0] = 0;
 
-    for (int i = 1; i < N; ++i) {
-        if (i == 1) dp[i] = abs(h[i]-h[i-1]);
-        else dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]),
-                         dp[i-2] + abs(h[i] - h[i-2]));
+    for (int i = 1; i < N; ++i){
+        chmin(dp[i], dp[i-1] + abs(h[i] - h[i-1]));
+        if (i > 1){
+            chmin(dp[i], dp[i-2] + abs(h[i] - h[i-2]));
+        }
     }
 
-    cout << dp[N-1] << endl;
+    cout << dp[N - 1] << endl;
+    return 0;
 }
+
+
+//int main(){
+//    int N;
+//    cin >> N;
+//    vector<long long>h(N);
+//    for (int i = 0; i < N; ++i) cin >> h[i];
+//
+//    vector<long long> dp(N, INF);
+//
+//    dp[0] = 0;
+//
+//    for (int i = 1; i < N; ++i) {
+//        if (i == 1) dp[i] = abs(h[i]-h[i-1]);
+//        else dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]),
+//                         dp[i-2] + abs(h[i] - h[i-2]));
+//    }
+//
+//    cout << dp[N-1] << endl;
+//}
 
 int GCD(int m, int n) {
     if (n == 0) return m;
