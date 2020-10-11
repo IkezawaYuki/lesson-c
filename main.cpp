9b124772ff65b6a0b886564736ccb1e2c27356b1
yikezawa <y.ikezawa93@gmail.com>
2020-10-11 19:07:58 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -3,33 +3,23 @@
 #include <vector>
 using namespace std;
 
-template<class T> void chmin(T& a, T b){
-    if (a > b){
-        a = b;
-    }
+
+void func(long long N, long long cur, int use, long long &counter){
+    if (cur > N) return;
+
+    if (use == 0b111) ++counter;
+
+    func (N, cur * 10 + 7, use | 0b001, counter);
+
+    func (N, cur * 10 + 5, use | 0b010, counter);
+
+    func (N, cur * 10 + 3, use | 0b100, counter);
 }
 
-const long long INF = 1LL << 60;
-
 int main(){
-    int N;
+    long long N;
     cin >> N;
-    vector<vector<long long>> c(N + 1, vector<long long>(N + 1));
-    for (int i = 0; i < N + 1; ++i){
-        for (int j = 0; j < N + 1; ++j){
-            cin >> c[i][j];
-        }
-    }
-
-    vector<long long> dp(N + 1, INF);
-
-    dp[0] = 0;
-
-    for (int i = 0; i <= N; ++i){
-        for (int j = 0; j < i; ++j){
-            chmin(dp[i], dp[j] + c[j][i]);
-        }
-    }
-
-    cout << dp[N] << endl;
+    long long counter = 0;
+    func(N, 0, 0, counter);
+    cout << counter << endl;
 }
