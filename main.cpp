f743a719d4daf5ef9bfa279200b82288decbdd8b
yikezawa <y.ikezawa93@gmail.com>
2020-10-11 18:03:38 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -9,29 +9,27 @@ template<class T> void chmin(T& a, T b){
     }
 }
 
-const int INF = 1 << 29;
+const long long INF = 1LL << 60;
 
 int main(){
-    string S, T;
-    cin >> S >> T;
-
-    vector<vector<int>> dp(S.size() + 1, vector<int>(T.size()+1, INF));
-    dp[0][0] = 0;
-
-    for (int i = 0; i <= S.size(); ++i){
-        for (int j = 0; j <= T.size(); ++j){
-            if (i > 0 && j > 0){
-                if (S[i-1] == T[j-1]){
-                    chmin(dp[i][j], dp[i-1][j-1]);
-                }else{
-                    chmin(dp[i][j], dp[i-1][j-1]+1);
-                }
-            }
-            if (i > 0) chmin(dp[i][j], dp[i-1][j]+1);
-
-            if (j > 0) chmin(dp[i][j], dp[i][j-1]+1);
+    int N;
+    cin >> N;
+    vector<vector<long long>> c(N + 1, vector<long long>(N + 1));
+    for (int i = 0; i < N + 1; ++i){
+        for (int j = 0; j < N + 1; ++j){
+            cin >> c[i][j];
         }
     }
 
-    cout << dp[S.size()][T.size()] << endl;
+    vector<long long> dp(N + 1, INF);
+
+    dp[0] = 0;
+
+    for (int i = 0; i <= N; ++i){
+        for (int j = 0; j < i; ++j){
+            chmin(dp[i], dp[j] + c[j][i]);
+        }
+    }
+
+    cout << dp[N] << endl;
 }
