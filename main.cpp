d3b791ea4f388ec160af5de6221c76a5f6efb4fc
yikezawa <y.ikezawa93@gmail.com>
2020-10-15 22:51:33 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 const int INF = 20000000;
 
+int calc(int);
+
 int main(){
     int N, A, B;
     cin >> N;
@@ -11,16 +13,20 @@ int main(){
     cin >> B;
     int counter = 0;
 
-    for (int i = 0; i < N; i++){
-        int tmp = 0;
-        if (tmp > 10){
-            tmp += i % 10 * 10;
-        }
-        tmp += i;
+    for (int i = 1; i <= N; i++){
+        int tmp = calc(i);
 
-        if (tmp >= A && tmp <= B) counter += tmp;
+        if (tmp >= A && tmp <= B) counter += i;
     }
 
     cout << counter << endl;
 }
 
+int calc(int a){
+    int sum = 0;
+    while (a > 0){
+        sum += a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
