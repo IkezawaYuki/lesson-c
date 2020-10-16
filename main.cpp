af97b4d604d0311e96b485c701fc1caed51a77a4
yikezawa <y.ikezawa93@gmail.com>
2020-10-16 23:24:10 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -2,31 +2,23 @@
 #include <string>
 #include <vector>
 using namespace std;
-const int INF = 20000000;
 
-int calc(int);
+const vector<int> value = {500, 100, 50, 10, 5, 1};
 
-int main(){
-    int N, A, B;
-    cin >> N;
-    cin >> A;
-    cin >> B;
-    int counter = 0;
+int main (){
+    int X;
+    vector<int>a(6);
+    cin >> X;
+    for(int i = 0; i < 6; ++i) cin >> a[i];
 
-    for (int i = 1; i <= N; i++){
-        int tmp = calc(i);
+    int result = 0;
+    for (int i = 0; i < 6; ++i) {
+        int add = X / value[i];
 
-        if (tmp >= A && tmp <= B) counter += i;
+        if (add > a[i]) add = a[i];
+
+        X -= value[i] * add;
+        result += add;
     }
-
-    cout << counter << endl;
-}
-
-int calc(int a){
-    int sum = 0;
-    while (a > 0){
-        sum += a % 10;
-        a = a / 10;
-    }
-    return sum;
+    cout << result << endl;
 }
