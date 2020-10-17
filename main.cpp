bd4de0a4f2f2577a6d9a5747f891e3fd3e616766
yikezawa <y.ikezawa93@gmail.com>
2020-10-17 22:19:51 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -11,5 +11,21 @@ bool cmp(const Interval &a, const Interval &b) {
 }
 
 int main(){
+    int N;
+    cin >> N;
+    vector<Interval> inter(N);
+    for (int i = 0; i < N; ++i){
+        cin >> inter[i].first >> inter[i].second;
+    }
+    sort(inter.begin(), inter.end(), cmp);
 
+    int res = 0;
+    int current_end_time = 0;
+    for (int i = 0; i < N; ++i){
+        if (inter[i].first < current_end_time) continue;
+
+        ++res;
+        current_end_time = inter[i].second;
+    }
+    cout << res << endl;
 }
