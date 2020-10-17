8edbfca0b59fb1094db100187b284e090d779342
yikezawa <y.ikezawa93@gmail.com>
2020-10-17 22:41:02 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,28 +4,19 @@
 #include <vector>
 using namespace std;
 
-using Interval = pair<int, int>;
-
-bool cmp(const Interval &a, const Interval &b) {
-    return a.second < b.second;
-}
-
 int main(){
     int N;
     cin >> N;
-    vector<Interval> inter(N);
-    for (int i = 0; i < N; ++i){
-        cin >> inter[i].first >> inter[i].second;
-    }
-    sort(inter.begin(), inter.end(), cmp);
+    vector<long long>A(N), B(N);
+    for (int i = 0; i < N; ++i) cin >> A[i] >> B[i];
 
-    int res = 0;
-    int current_end_time = 0;
-    for (int i = 0; i < N; ++i){
-        if (inter[i].first < current_end_time) continue;
-
-        ++res;
-        current_end_time = inter[i].second;
+    long long sum = 0;
+    for (int i = N - 1; i >= 0; --i) {
+        A[i] += sum;
+        long long amari = A[i] % B[i];
+        long long D = 0;
+        if (amari != 0) D = B[i] - amari;
+        sum += D;
     }
-    cout << res << endl;
+    cout << sum << endl;
 }
