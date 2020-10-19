d6f6124681929ddf416401fb4b10d24aaf753402
yikezawa <y.ikezawa93@gmail.com>
2020-10-19 22:50:49 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,64 +4,41 @@
 #include <vector>
 using namespace std;
 
-struct Node{
-    Node *next, *prev;
-    string name;
+const int MAX = 100000;
 
-    Node(string name_ = "") :
-    prev(NULL), next(NULL), name(name_) { }
-};
-
-Node* nil;
+int st[MAX];
+int top = 0;
 
 void init(){
-    nil = new Node();
-    nil->next = nil;
-    nil->prev = nil;
+    top = 0;
 }
 
-void printList(){
-    Node* cur = nil->next;
-    for(; cur != nil; cur = cur->next){
-        cout << cur->name << " -> ";
+bool isEmpty(){
+    return (top == 0);
+}
+
+bool isFull(){
+    return (top == MAX);
+}
+
+void push(int x){
+    if (isFull()){
+        cout << "error: stack is full." << endl;
+        return;
     }
-    cout << endl;
+    st[top] = x;
+    ++top;
 }
 
-void insert(Node* v, Node* p = nil){
-    v->next = p->next;
-    p->next->prev = v;
-    p->next = v;
-    v->prev = p;
-}
-
-void erase(Node *v){
-    if (v == nil) return;
-    v->prev->next = v->next;
-    v->next->prev = v->prev;
-    delete v;
+int pop(){
+    if (isEmpty()){
+        cout << "error: stack is empty" << endl;
+        return -1;
+    }
+    --top;
+    return st[top];
 }
 
 int main(){
-    init();
-
-    vector<string> names = {"yamamoto", "watanabe", "ito", "takahashi", "suzuki", "sato"};
-
-    Node *watanabe;
-    for (int i = 0; i < (int) names.size(); ++i){
-        Node* node = new Node(names[i]);
-
-        insert(node);
-
-        cout << "step " << i << ": ";
-        if (names[i] == "watanabe") watanabe = node;
-        printList();
-    }
-
-    cout << "before: ";
-    printList();
-    erase(watanabe);
-    cout << "after: ";
-    printList();
 
 }
