7d6bee8773e63d827ee166f2d5b2839b0ca98f2b
yikezawa <y.ikezawa93@gmail.com>
2020-10-24 12:59:06 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,46 +4,27 @@
 #include <vector>
 using namespace std;
 
-struct UnionFind{
-    vector<int> par, siz;
+void InsertionSort(vector<int> &a){
+    int N = (int) a.size();
+    for (int i = 0; i < N; ++i){
+        int v = a[i];
 
-    UnionFind(int n) : par(n, -1), siz(n, 1) { }
-
-    int root(int x){
-        if (par[x] == -1) return x;
-        else return par[x] = root(par[x]);
+        int j = i;
+        for (; j > 0; --j){
+            if (a[j-1] > v){
+                a[j] = a[j-1];
+            }
+            else break;
+        }
+        a[j] = v;
     }
-
-    bool issame(int x, int y) {
-        return root(x) == root(y);
-    }
-
-    bool unite(int x, int y){
-        x = root(x); y = root(y);
-
-        if(x == y) return false;
-
-        if (siz[x] < siz[y]) swap(x, y);
-
-        par[y] = x;
-        siz[x] += siz[y];
-        return true;
-    }
-
-    int size(int x){
-        return siz[root(x)];
-    }
-};
+}
 
 int main(){
-    UnionFind uf(7);
+    int N;
+    cin >> N;
+    vector<int>a(N);
+    for (int i = 0; i < N; ++i) cin >> a[i];
 
-    uf.unite(1, 2);
-    uf.unite(2, 3);
-    uf.unite(5, 6);
-    cout << uf.issame(1, 3) << endl;
-    cout << uf.issame(2, 5) << endl;
-
-    uf.unite(1, 6);
-    cout << uf.issame(2, 5) << endl;
+    InsertionSort(a);
 }
