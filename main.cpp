82e6bae84b157d42bdd5a3205d592229ffb6a082
yikezawa <y.ikezawa93@gmail.com>
2020-10-24 13:56:35 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,30 +4,37 @@
 #include <vector>
 using namespace std;
 
-void QuickSort(vector<int> &a, int left, int right){
-    if (right - left <= 1) return;
+void Heapify(vector<int> &a, int i, int N){
+    int child1 = i * 2 + 1;
+    if (child1 >= N) return;
 
-    int pivot_index = (left + right) / 2;
-    int pivot = a[pivot_index];
-    swap(a[pivot_index], a[right-1]);
+    if (child1 + 1 < N && a[child1 + 1] > a[child1]) ++child1;
 
-    int i = left;
-    for(int j = left; j < right - 1; ++j){
-        if (a[j] < pivot){
-            swap(a[i++], a[j]);
-        }
+    if (a[child1] <= a[i]) return;
+
+    swap(a[i], a[child1]);
+
+    Heapify(a, child1, N);
+}
+
+void HeapSort(vector<int> &a){
+    int N = (int)a.size();
+
+    for(int i = N / 2 - 1; i >= 0; --i){
+        Heapify(a, i, N);
     }
-    swap(a[i], a[right-1]);
 
-    QuickSort(a, left, i);
-    QuickSort(a, i+1, right);
+    for(int i = N - 1; i > 0; --i){
+        swap(a[0], a[i]);
+        Heapify(a, 0, i);
+    }
 }
 
 int main(){
     int N;
     cin >> N;
     vector<int>a(N);
-    for(int i = 0; i < N; ++i) cin >> a[i];
+    for (int i = 0; i < N; ++i) cin >> a[i];
 
-    QuickSort(a, 0, N);
+    HeapSort(a);
 }
