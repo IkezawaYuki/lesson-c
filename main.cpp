d158a5f883b9dc3a5c42287fd9b0c45e47b97396
yikezawa <y.ikezawa93@gmail.com>
2020-10-24 15:47:00 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -4,32 +4,55 @@
 #include <vector>
 #include <queue>
 using namespace std;
-
 using Graph = vector<vector<int>>;
 
-vector<bool> seen;
+vector<int> BFS(const Graph &G, int s){
+    int N = (int)G.size();
+    vector<int>dist(N, -1);
+    queue<int> que;
 
-void dfs(const Graph &G, int v) {
-    seen[v] = true;
+    dist[0] = 0;
+    que.push(0);
 
-    for (auto next_v : G[v]){
-        if (seen[next_v]) continue;
-        dfs(G, next_v);
+    while(!que.empty()){
+        int v = que.front();
+        que.pop();
+
+        for (int x : G[v]) {
+            if (dist[x] != -1)continue;
+
+            dist[x] = dist[v] + 1;
+            que.push(x);
+        }
     }
+    return dist;
 }
+
 
 int main(){
     int N, M;
     cin >> N >> M;
     Graph G(N);
+
     for (int i = 0; i < M; ++i){
         int a, b;
         cin >> a >> b;
         G[a].push_back(b);
+        G[b].push_back(a);
     }
-    seen.assign(N, false);
-    for (int v = 0; v < N; ++v){
-        if (seen[v]) continue;
-        dfs(G, v);
-    }
+
+    vector<int> dist = BFS(G, 0);
+
+    for (int v = 0; v < N; ++v) cout << v << ": " << dist[v] << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
