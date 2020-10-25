44adcea4bf25de37484e72f74bb87364ac7b8594
yikezawa <y.ikezawa93@gmail.com>
2020-10-25 14:32:36 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -6,39 +6,36 @@
 using namespace std;
 using Graph = vector<vector<int>>;
 
-vector<int> color;
-bool dfs(const Graph &G, int v, int cur = 0){
-    color[v] = cur;
-    for (auto next_v: G[v]){
-        if (color[next_v] != -1){
-            if (color[next_v] == cur) return false;
-
-            continue;
-        }
-
-        if (!dfs(G, next_v, 1 - cur)) return false;
+vector<bool> seen;
+vector<int> order;
+void rec(const Graph &G, int v){
+    seen[v] = true;
+    for (auto next_v : G[v]){
+        if (seen[next_v]) continue;
+        rec(G, next_v);
     }
-    return true;
+
+    order.push_back(v);
 }
 
 int main(){
     int N, M;
     cin >> N >> M;
-
     Graph G(N);
     for (int i = 0; i < M; ++i){
         int a, b;
         cin >> a >> b;
         G[a].push_back(b);
-        G[b].push_back(a);
     }
 
-    color.assign(N, -1);
-    bool is_bipartite = true;
+    seen.assign(N, false);
+    order.clear();
     for (int v = 0; v < N; ++v){
-        if (color[v] != -1) continue;
-        if (!dfs(G, v)) is_bipartite = false;
+        if (seen[v]) continue;
+        rec(G, v);
     }
-    if (is_bipartite) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    reverse(order.begin(), order.end());
+
+    for (auto v : order) cout << v << " -> ";
+    cout << endl;
 }
